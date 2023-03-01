ee5033a8861cf64751f308f4e5c3d9cc6788a24d
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-01 14:06:42 -0300

    jest reset

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  bail: true, // se um teste falhar ele para de executar o teste
-  coverageProvider: 'v8',
-  // Vale a pena colocar, pois quando o teste for executar ele vai ignorar os outros arquivos e vai direto nos arquivos de teste
-  testMatch: [ // expressão regular para disser o padrão dos arquivos de teste
-    "<rootDir>/src/**/*.spec.js"  // dentro de qualquer pasta vai ter um arquivo de qualquer nome com a extensão .spec.js
-  ] //(.spec.js/.test.js)-> para falar que é um arquivo de test
-};
